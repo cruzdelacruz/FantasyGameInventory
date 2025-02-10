a8e6e1827eeaa7f930fac083740fe1e98e7a0c69
Cruz Delacruz <cruz.delacruz37@gmail.com>
2025-02-09 23:18:03 -0500

    Update item_class.py
    Adding to item clasa

--- item_class.py
+++ item_class.py
@@ -1,3 +1,9 @@
 class game_item():
-    def __init__(self):
+    def __init__(self, name: str, quantity: int, weight: float):
+        self.name = name
+        self.quantity = quantity
+        self.weight = weight
+        
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name},quantity={sel.quantity},weight={self.weight}kg)"
       
